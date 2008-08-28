ed5ee38ed2076e134ac188ec457d1b42f7fddd87
Victor Ng <victor@monkeybean.ca>
2008-08-28 10:18:23 -0400

    ignore .orig files from hg revert
    added a test module with doctests in it

---
+++ examples/project/zoo/test_foo.py
@@ -0,0 +1,12 @@
+
+def test_regular_test_method():
+    ''' This is just a stub for a regular test method'''
+    pass
+
+def blah():
+    '''
+    This is a doctest that might get skipped
+
+    >>> 5 + 5 
+    10
+    '''
