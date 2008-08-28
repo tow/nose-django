f31aaf2854d40735f1e4d8977d3dd2714964f247
Victor Ng <victor@monkeybean.ca>
2008-08-28 09:52:49 -0400

    upped the revision to 0.6 to match Django 1.0b1

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='NoseDjango',
-    version='0.5',
+    version='0.6',
     author='Victor Ng',
-    author_email = 'victor.ng@monkeybeanonline.com',
+    author_email = 'victor@monkeybean.ca',
     description = 'nose plugin for easy testing of django projects ' \
         'and apps. Sets up a test database (or schema) and installs apps ' \
         'from test settings file before tests are run, and tears the test ' \
         'database (or schema) down after all tests are run.',
     install_requires='nose>=0.10',
-    url = "http://poli-cms.googlecode.com/svn/nose-django/",
+    url = "http://www.assembla.com/spaces/nosedjango",
     license = 'GNU LGPL',
     packages = find_packages(exclude=['tests']),
     zip_safe = False,
